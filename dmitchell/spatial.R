03b219543be81307a57aea6224c966435f1cae0e
Daniel Mitchell <daniel.lewis.mitchell@gmail.com>
2015-03-13 11:55:55 -0500

    Added option for scaling observations
    This should resolve some numeric optimization issues.

--- dmitchell/spatial.R
+++ dmitchell/spatial.R
@@ -17,7 +17,7 @@ matern <- function(d, tausq, b){ d <- sqrt(5)*d/b; tausq*(1+d+d*d/3)*exp(-d) }
 #   theta is null or a list of hyperparameters (tausq, b, sigsq)
 #
 # returns hyperparameters and the posterior mean and SD at prediction locations
-smooth <- function(y, x, newx, cov.fun, theta=NULL){
+smooth <- function(y, x, newx, cov.fun, theta=NULL, scale=TRUE){
   n <- length(x)
   m <- length(newx)
 
@@ -27,15 +27,16 @@ smooth <- function(y, x, newx, cov.fun, theta=NULL){
   for(i in 1:m) s[,n+i] <- spDistsN1(x, newx[i,], longlat=T)
 
   # negative log marginal likelihood for empirical bayes
-  objective <- function(u){
+  objective <- function(u, z){
     tausq <- exp(u[1]); b <- exp(u[2]); sigsq <- exp(u[3])
     cmat <- cov.fun(s[1:n,1:n], tausq, b) + diag(sigsq, n)
-    log(det(cmat)) + t(y) %*% solve(cmat) %*% y
+    log(det(cmat)) + t(z) %*% solve(cmat) %*% z
   }
 
   # use empirical bayes to estimate hyperparameters if none given
   if(is.null(theta)){
-    u <- exp(optim(c(0,0,0), objective)$par)
+    if(scale) z <- scale(y) else z <- y
+    u <- exp(optim(c(0,0,0), function(u) objective(u,z))$par)
     theta <- list(tausq=u[1], b=u[2], sigsq=u[3])
   }
 
@@ -63,21 +64,17 @@ demo <- function(){
 
   # create output points
   R <- brick(extent(weather), 200, 200, nl=4)
-  newx <- rasterToPoints(R, spatial=T)
+  newx <- rasterToPoints(R, spatial=TRUE)
 
   # smooth temperature
-  sm <- smooth(scale(weather$temperature), weather, newx, matern)
-  m <- mean(weather$temperature)
-  s <- sd(weather$temperature)
-  R <- setValues(R, m+s*sm$mean, layer=1)
-  R <- setValues(R, s*sm$sd, layer=2)
+  sm <- smooth(weather$temperature, weather, newx, matern)
+  R <- setValues(R, sm$mean, layer=1)
+  R <- setValues(R, sm$sd, layer=2)
 
   # smooth pressure
-  sm <- smooth(scale(weather$pressure), weather, newx, matern)
-  m <- mean(weather$pressure)
-  s <- sd(weather$pressure)
-  R <- setValues(R, m+s*sm$mean, layer=3)
-  R <- setValues(R, s*sm$sd, layer=4)
+  sm <- smooth(weather$pressure, weather, newx, matern)
+  R <- setValues(R, sm$mean, layer=3)
+  R <- setValues(R, sm$sd, layer=4)
 
   return(R)
 }
@@ -89,7 +86,7 @@ spectral.colors <- colorRampPalette(rev(brewer.pal(11, "Spectral")))
 heat.colors <- colorRampPalette(rev(brewer.pal(9, "YlOrRd")))
 
 R <- demo()
-levelplot(R, layer=1, col.regions=spectral.colors, contour=T, margin=F, main="Temperature")
-levelplot(R, layer=2, col.regions=heat.colors, contour=T, margin=F, main="Temperature standard deviation")
-levelplot(R, layer=3, col.regions=spectral.colors, contour=T, margin=F, main="Pressure")
-levelplot(R, layer=4, col.regions=heat.colors, contour=T, margin=F, main="Pressure standard deviation")
+levelplot(R, layer=1, col.regions=spectral.colors, contour=T, margin=F, xlab="Longitude", ylab="Latitude", main="Temperature")
+levelplot(R, layer=2, col.regions=heat.colors, contour=T, margin=F, xlab="Longitude", ylab="Latitude", main="Temperature standard deviation")
+levelplot(R, layer=3, col.regions=spectral.colors, contour=T, margin=F, xlab="Longitude", ylab="Latitude", main="Pressure")
+levelplot(R, layer=4, col.regions=heat.colors, contour=T, margin=F, xlab="Longitude", ylab="Latitude", main="Pressure standard deviation")
